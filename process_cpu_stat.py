f92eb2e0e9a241a0971860bce3330504101be8fc
Joy Rahman <joy@Joys-MacBook-Air-2.local>
2014-12-17 22:48:54 -0600

    updated and fixe

--- process_cpu_stat.py
+++ process_cpu_stat.py
@@ -11,36 +11,37 @@ __all__ = ['PROCCPU','get_cpu_usage']
 _instance = None
 
 def _get_instance(pid='None'):
-	global _instance
-	if _instance is None:
-		_instance = PROCCPU(pid)
-	return _instance
-	
+    global _instance
+    if _instance is None:
+        _instance = PROCCPU(pid)
+    return _instance
+
 def get_cpu_usage():
-	return _get_instance().get()
+    return _get_instance().get()
 
 
 class  PROCCPU(object):
-	"""docstring for  CPUUSage"""
-	def __init__(self, pid):
-		self.pid = pid
-		self.stat_path = "/proc/"+pid+"/stat"
+    """docstring for  CPUUSage"""
+    def __init__(self, pid):
+        self.pid = pid
+        self.stat_path = "/proc/"+pid+"/stat"
 
-	def get(self):
-		return self.parse_process_cpu_usage(self.read_process_cpu_usage(self.stat_path))
-	__call__ = get		
-
-	@staticmethod
-	def read_process_cpu_usage(stat_path):
-	    with open(stat_path) as stat_file:
-    		return next(stat_file)
+    def get(self):
+        return self.parse_process_cpu_usage(self.read_process_cpu_usage(self.stat_path))
+    __call__ = get
 
     @staticmethod
-	def parse_process_cpu_usage(cpu_stat_line):
-		token = cpu_stat_line.split()
-		utime = token[13] 
-		stime = token[14]  
-    	return (utime,stime)
+    def read_process_cpu_usage(stat_path):
+        with open(stat_path) as stat_file:
+            output = stat_file.readline()
+            return output
+
+    @staticmethod
+    def parse_process_cpu_usage(cpu_stat_line):
+        token = cpu_stat_line.split()
+        utime = token[13]
+        stime = token[14]
+        return (utime,stime)
 
 
 
--- system_cpu_stat.py
+++ system_cpu_stat.py
@@ -11,31 +11,35 @@ __all__ = ['SYSTEMCPU','get_cpu_usage']
 _instance = None
 
 def _get_instance():
-	global _instance
-	if _instance is None:
-		_instance = SYSTEMCPU()
-	return _instance
-	
+    global _instance
+    if _instance is None:
+        _instance = SYSTEMCPU()
+    return _instance
+
 def get_cpu_usage():
-	return _get_instance().get()
-			
+    return _get_instance().get()
+
 
 class  SYSTEMCPU(object):
-	"""docstring for  SYSTEMCPU"""
-	def __init__(self, stat_path='/proc/stat'):
-		self.stat_path = stat_path
+    """docstring for  SYSTEMCPU"""
+    def __init__(self, stat_path='/proc/stat'):
+        self.stat_path = stat_path
 
-	def get(self):
-		return self.parse_system_cpu_usage(self.read_system_cpu_usage(self.stat_path))
-	__call__ = get		
+    def get(self):
+        return self.parse_system_cpu_usage(self.read_system_cpu_usage(self.stat_path))
 
-	@staticmethod
-	def read_system_cpu_usage(stat_path):
-    	with open(stat_path) as stat_file:
-    		return next(stat_file)
+    __call__ = get
+
 
     @staticmethod
-	def parse_system_cpu_usage(cpu_stat_line):  
-    	return sum(float(time) for time in islice(cpu_stat_line.split(),1,None))
+    def read_system_cpu_usage(stat_path):
+        with open(stat_path) as stat_file:
+            output = stat_file.readline()
+        return next(stat_file)
+
+    @staticmethod
+    def parse_system_cpu_usage(cpu_stat_line):
+        return sum(float(time) for time in islice(cpu_stat_line.split(),1,None))
+
 
 
