90250cdab6b34b527d722be1d73a1cea29c5f9b4
Ubuntu <ubuntu@ansible-control.novalocal>
2015-01-05 05:01:18 +0000

    git updated

--- deamon.py
+++ deamon.py
@@ -1,11 +1,17 @@
-import proc_to_pid 
+import proc_to_pid
 import sys
 import collect_stat as proc_data
 import threading
 import logging
 import time
+from Queue import Queue
+
 logging.basicConfig(level=logging.DEBUG,\
         format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
+
+
+threads = []
+q = Queue()
 
 def worker(pid, proc_name, timer=120):
     logging.debug('Starting')
@@ -14,15 +20,19 @@ def worker(pid, proc_name, timer=120):
 
 
 def run_threads(pids):
-    threads = []
+    #threads = []
+    global threads
+    for t in threads:
+        print t
     for pid in pids:
         thread_name = "{}-{}".format(str(pid), proc_name)
-        t = threading.Thread(name= thread_name, target=worker,\
-                args=(pid, proc_name, timer))
-        threads.append(t)
-        t.start()
-    return threads    
-                                                
+        if thread_name in threading.enumerate():
+            print "Thread Exist"
+        else:
+            t = threading.Thread ( name = thread_name, target = worker, args = ( pid, proc_name, timer ))
+            threads.append(t)
+            t.start()
+    #return threads    
 
 
 def pid_exist(proc_name):
@@ -32,42 +42,65 @@ def pid_exist(proc_name):
     else:
         return true
 
-if __name__ == "__main__":
 
+def main():
     proc_name = "apache"
     timer =  13
+    
+    # get the matching pids to proc_name
+    pids =  proc_to_pid.get_pid(proc_name)
+    
+    '''
+    if pid is not null, then insert the pid to the queue
+    '''
+    
+    if (pid !=-1) and q.empty():
+        for item in pid:
+            q.put(item)
 
+  
+
+if __name__ == "__main__":
+    
+    main()
+    
+    '''
+    proc_name = "apache"
+    timer =  13
     if len(sys.argv)>=2:
         proc_name = sys.argv[1]
-    
     thread_running =  False
     try:
         while (True):
             pids = proc_to_pid.get_pid(proc_name)
             if (pids != -1)  and (thread_running == False) :
                 thread_running = True
-                thread_list = run_threads(pids)         
+                #thread_list = run_threads(pids)         
+                run_threads(pids)
                 #main_thread = threading.currentThread()
                 time.sleep(2*timer)
+                '''
+                # this section is disabled
                 '''
                 for t in threading.enumerate():
                     if t is main_thread:
                         continue
                     logging.debug('joining %s', t.getName())
                     t.join()
-                '''
-            '''    
             elif pids!= -1 and thread_running == True:
                 print "pass"
                 if threading.active_count() == 1:
                     thread_running = False 
+                 '''
+            #temporary disabled
+
             '''
             else:
                 #thread_running = False
                 if threading.active_count() == 1:
                     thread_running = False
                 time.sleep(1)
-
+    
     except KeyboardInterrupt:
         print "Existing"
         #sys.exit() 
@@ -78,9 +111,10 @@ if __name__ == "__main__":
         main_thread = threading.currentThread()
         for t in threading.enumerate():
             if t is main_thread:
-               continue
+                continue
             logging.debug('joining %s', t.getName())
             t.join()
+    '''        
 '''
     # list of existing pids
     pids = proc_to_pid.get_pid(proc_name)
