b9966bafe61d57c7da2aa078687299ce4adbad64
joyrahman <joyrahman@gmail.com>
2015-01-05 00:14:58 -0600

    SetQueue

---
+++ SetQueue.py
@@ -0,0 +1,7 @@
+class SetQueue(Queue.Queue):
+    def _init(self, maxsize):
+        self.queue = set()
+    def _put(self, item):
+        self.queue.add(item)
+    def _get(self):
+        return self.queue.pop()
